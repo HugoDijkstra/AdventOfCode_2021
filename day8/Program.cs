cea7c2e349fbf0f7d872dd441169c481964b5d6f
Hugo Dijkstra <hugodijkstra@live.nl>
2021-12-09 13:29:19 +0100

    Day 9

---
+++ day8/Program.cs
@@ -0,0 +1,175 @@
+﻿namespace Program
+{
+    public static class Program
+    {
+        class Display
+        {
+            public char top, botom, topLeft, topRight, middle, bottomLeft, BottomRight;
+        }
+
+        // A not in B
+        public static char notIn(string a, string b)
+        {
+            foreach (var c in a)
+            {
+                if (!b.Contains(c))
+                    return c;
+            }
+            return '\0';
+        }
+
+        public static int difference(string a, string b)
+        {
+            int dif = 0;
+            foreach (var c in a)
+            {
+                if (!b.Contains(c))
+                {
+                    dif++;
+                }
+            }
+            return dif;
+        }
+        public static void Main()
+        {
+            string[] input = File.ReadAllLines("input.txt");
+            int unique = 0;
+            int total = 0;
+            foreach (var inp in input)
+            {
+                if (string.IsNullOrEmpty(inp))
+                    continue;
+
+                string[] split = inp.Split('|');
+                List<string> segments = new(split[0].Split(' '));
+                string[] usedInput = split[1].Split(' ');
+                Display display = new Display();
+                SortedList<int, string> numbers = new();
+
+                //Get known
+                for (int i = 0; i < segments.Count; i++)
+                {
+                    if (segments[i].Length == 7)
+                    {
+                        numbers.Add(8, segments[i]);
+                    }
+                    else if (segments[i].Length == 2)
+                    {
+                        numbers.Add(1, segments[i]);
+                    }
+                    else if (segments[i].Length == 4)
+                    {
+                        numbers.Add(4, segments[i]);
+                    }
+                    else if (segments[i].Length == 3)
+                    {
+                        numbers.Add(7, segments[i]);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                    segments.RemoveAt(i);
+                    i--;
+                }
+
+                foreach (var seg in segments)
+                {
+                    if (seg.Length == 5)
+                    {
+                        char c = notIn(numbers[1], seg);
+                        if (c == '\0')
+                        {
+                            numbers.Add(3, seg);
+                        }
+                    }
+                }
+                segments.Remove(numbers[3]);
+                //Set top
+                char t = notIn(numbers[7], numbers[1]);
+                Console.WriteLine(t);
+                display.top = t;
+
+                foreach (var seg in segments)
+                {
+                    if (seg.Length == 6)
+                    {
+                        char c = notIn(numbers[3], seg);
+                        if (c == '\0')
+                        {
+                            numbers.Add(9, seg);
+                            break;
+                        }
+                    }
+                }
+                segments.Remove(numbers[9]);
+                foreach (var seg in segments)
+                {
+                    if (seg.Length == 6)
+                    {
+                        char c = notIn(numbers[4], seg);
+                        if (c != '\0')
+                        {
+                            c = notIn(numbers[1], seg);
+                            if (c != '\0')
+                            {
+                                numbers.Add(6, seg);
+                                break;
+                            }
+                        }
+                    }
+                }
+                segments.Remove(numbers[6]);
+                foreach (var seg in segments)
+                {
+                    if (seg.Length == 6)
+                    {
+                        numbers.Add(0, seg);
+                    }
+                }
+                segments.Remove(numbers[0]);
+
+                foreach (var seg in segments)
+                {
+                    if (seg.Length == 5)
+                    {
+                        char c = notIn(seg, numbers[6]);
+                        if (c == '\0')
+                        {
+                            numbers.Add(5,seg);
+                        }
+                    }
+                }
+                segments.Remove(numbers[5]);
+                foreach (var seg in segments)
+                {
+                    if (seg.Length == 5)
+                    {
+                        char c = notIn(seg, numbers[6]);
+                        if (c != '\0')
+                        {
+                            numbers.Add(2,seg);
+                        }
+                    }
+                }
+                segments.Remove(numbers[2]);
+                int times = 1000;
+                foreach (var part in usedInput)
+                {
+                    string sorted = string.Concat(part.OrderBy(c=>c));
+                    for(int i = 0; i < numbers.Count;i++)
+                    {
+                        string num = string.Concat( numbers[i].OrderBy(c=>c));
+                        if(sorted ==  num)
+                        {
+                            total += i*times;
+                            Console.WriteLine(i);
+                            times /= 10;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
---
+++ day9/Program.cs
@@ -0,0 +1,107 @@
+﻿namespace Program
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            string input = File.ReadAllText("input.txt").Trim();
+            List<List<int>> allHeights = new();
+            int totalDanger = 0;
+            List<int> curList = new();
+            allHeights.Add(curList);
+            List<List<Vector2>> basins = new();
+            foreach (char c in input)
+            {
+                //stupid hack
+                if (c == '\n')
+                {
+                    curList = new();
+                    allHeights.Add(curList);
+                    continue;
+                }
+                string s = c + "";
+                curList.Add(int.Parse(s));
+            }
+
+            for (int i = 0; i < allHeights.Count - 0; i++)
+            {
+                for (int j = 0; j < allHeights[i].Count; j++)
+                {
+                    if (allHeights[i][j] != 9 && allHeights[i][j] != -1)
+                    {
+                        //Use fill algorithm
+                        List<Vector2> basin = findAllConnectedPositions(ref allHeights, new Vector2() { x = i, y = j });
+                        basins.Add(basin);
+                    }
+                }
+                // Part 1
+                // for (int j = 0; j < allHeights[i].Count; j++)
+                // {
+                //     if (i == 0 || allHeights[i - 1][j] > allHeights[i][j])
+                //         if (i == allHeights.Count - 1 || allHeights[i + 1][j] > allHeights[i][j])
+                //             if (j == 0 || allHeights[i][j - 1] > allHeights[i][j])
+                //                 if (j == allHeights[i].Count - 1 || allHeights[i][j + 1] > allHeights[i][j])
+                //                 {
+                //                     totalDanger += allHeights[i][j] + 1;
+                //                 }
+                // }
+            }
+            for (int i = 0; i < allHeights.Count - 0; i++)
+            {
+                for (int j = 0; j < allHeights[i].Count; j++)
+                    Console.Write(allHeights[i][j] == 9 ? 9 : 0);
+                Console.Write('\n');
+            }
+
+            basins.Sort(delegate (List<Vector2> a, List<Vector2> b)
+            {
+                if (a.Count == b.Count) return 0;
+                return a.Count > b.Count ? -1:1;
+
+            });
+
+            Console.WriteLine(basins[0].Count * basins[1].Count * basins[2].Count);
+        }
+
+        public static List<Vector2> findAllConnectedPositions(ref List<List<int>> allHeights, Vector2 startPos)
+        {
+            List<Vector2> ret = new();
+            ret.Add(startPos);
+            allHeights[startPos.x][startPos.y] = -1;
+            if (startPos.x != 0)
+            {
+                if (allHeights[startPos.x - 1][startPos.y] != 9
+                && allHeights[startPos.x - 1][startPos.y] != -1)
+                    ret.AddRange(findAllConnectedPositions(ref allHeights, new Vector2() { x = startPos.x - 1, y = startPos.y }));
+            }
+            if (startPos.x <= allHeights.Count - 2)
+            {
+                if (allHeights[startPos.x + 1][startPos.y] != 9
+                && allHeights[startPos.x + 1][startPos.y] != -1)
+                    ret.AddRange(findAllConnectedPositions(ref allHeights, new Vector2() { x = startPos.x + 1, y = startPos.y }));
+            }
+            if (startPos.y != 0)
+            {
+                if (allHeights[startPos.x][startPos.y - 1] != 9
+                && allHeights[startPos.x][startPos.y - 1] != -1)
+                    ret.AddRange(findAllConnectedPositions(ref allHeights, new Vector2() { x = startPos.x, y = startPos.y - 1 }));
+            }
+            if (startPos.y <= allHeights[startPos.x].Count - 2)
+            {
+                if (allHeights[startPos.x][startPos.y + 1] != 9
+                && allHeights[startPos.x][startPos.y + 1] != -1)
+                    ret.AddRange(findAllConnectedPositions(ref allHeights, new Vector2() { x = startPos.x, y = startPos.y + 1 }));
+            }
+            return ret;
+        }
+        public class Vector2 : IEquatable<Vector2>
+        {
+            public int x, y;
+
+            public bool Equals(Vector2? other)
+            {
+                return other != null && x == other.x && y == other.y;
+            }
+        }
+    }
+}
